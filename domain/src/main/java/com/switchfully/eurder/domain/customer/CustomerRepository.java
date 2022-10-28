4079a5ab84d5fe797c457adaf0480cbc802f2f22
dominiquevaneyken <dominique.van.eyken@telenet.be>
2022-10-28 14:20:19 +0200

    unit testing for customerRepository

--- domain/src/main/java/com/switchfully/eurder/domain/customer/CustomerRepository.java
+++ domain/src/main/java/com/switchfully/eurder/domain/customer/CustomerRepository.java
@@ -1,9 +1,9 @@
 package com.switchfully.eurder.domain.customer;
 
+import com.switchfully.eurder.domain.address.Address;
 import com.switchfully.eurder.domain.address.AddressBuilder;
-import com.switchfully.eurder.domain.customer.Customer;
-import com.switchfully.eurder.domain.customer.CustomerBuilder;
-import com.switchfully.eurder.domain.customer.Role;
+import com.switchfully.eurder.domain.phonenumber.CountryCode;
+import com.switchfully.eurder.domain.phonenumber.PhoneNumber;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
@@ -16,17 +16,7 @@ public class CustomerRepository {
 
     public CustomerRepository() {
         this.customerRepository = new HashMap<>();
-        Customer admin = new CustomerBuilder()
-                .setLastname("admin")
-                .setEmailAddress("admin@eurder.com")
-                .setPassword("password")
-                .setAddress(new AddressBuilder()
-                        .setPostalCode("0000")
-                        .setCityName("EURDER")
-                        .build())
-                .setRole(Role.ADMIN)
-                .build();
-        customerRepository.put(admin.getCustomerID(), admin);
+        fillCustomerRepository();
     }
 
     public void addCustomer(Customer customer) {
@@ -44,5 +34,25 @@ public class CustomerRepository {
             }
         }
         throw new NoSuchElementException("Wrong credentials");
+    }
+
+    private void fillCustomerRepository() {
+        Customer admin = new CustomerBuilder()
+                .setLastname("admin")
+                .setEmailAddress("admin@eurder.com")
+                .setPassword("password")
+                .setAddress(new AddressBuilder()
+                        .setPostalCode("0000")
+                        .setCityName("EURDER")
+                        .build())
+                .setRole(Role.ADMIN)
+                .build();
+        customerRepository.put(admin.getCustomerID(), admin);
+        Customer customer1 = new Customer("firstname1", "lastname1", "user1@test.be", new Address("street",  "1","1111", "city1"), new PhoneNumber(CountryCode.BEL, "123 45 67 89"), "password", Role.CUSTOMER);
+        Customer customer2 = new Customer("firstname2", "lastname2", "user2@test.be", new Address("street",  "1","1111", "city2"), new PhoneNumber(CountryCode.BEL, "123 45 67 89"), "password", Role.CUSTOMER);
+        Customer customer3 = new Customer("firstname3", "lastname3", "user3@test.be", new Address("street",  "1","1111", "city3"), new PhoneNumber(CountryCode.BEL, "123 45 67 89"), "password", Role.CUSTOMER);
+        customerRepository.put(customer1.getCustomerID(), customer1);
+        customerRepository.put(customer2.getCustomerID(), customer2);
+        customerRepository.put(customer3.getCustomerID(), customer3);
     }
 }
--- domain/src/test/java/com/switchfully/eurder/domain/CustomerRepositoryTest.java
+++ domain/src/test/java/com/switchfully/eurder/domain/CustomerRepositoryTest.java
@@ -6,22 +6,46 @@ import com.switchfully.eurder.domain.customer.CustomerRepository;
 import com.switchfully.eurder.domain.customer.Role;
 import com.switchfully.eurder.domain.phonenumber.CountryCode;
 import com.switchfully.eurder.domain.phonenumber.PhoneNumber;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class CustomerRepositoryTest {
+    private CustomerRepository customerRepository;
+    private final Customer testCustomer = new Customer("firstname", "lastname", "user@test.be", new Address("street",  "1","1111", "city"), new PhoneNumber(CountryCode.BEL, "123 45 67 89"), "password", Role.CUSTOMER);
 
-    private final Customer customer = new Customer("firstname", "lastname", "user@test.be", new Address("street",  "1","1111", "city"), new PhoneNumber(CountryCode.BEL, "123 45 67 89"), "password", Role.CUSTOMER);
+    @BeforeEach
+    void createAndFillCustomerRepository() {
+        customerRepository = new CustomerRepository();
+    }
+
+
 
     @Test
     void addCustomerToRepository() {
-        CustomerRepository customerRepository = new CustomerRepository();
         int before = customerRepository.getAmountOfCustomersInRepository();
-        customerRepository.addCustomer(customer);
+        customerRepository.addCustomer(testCustomer);
         int after = customerRepository.getAmountOfCustomersInRepository();
 
         assertThat(before).isLessThan(after);
+    }
+
+    @Test
+    void getCustomerByEmail_givenValidEmailAddress() {
+        customerRepository.addCustomer(testCustomer);
+        Customer result = customerRepository.getMemberByEmail("user@test.be");
+
+        assertThat(result).isEqualTo(testCustomer);
+    }
+    @Test
+    void getCustomerByEmail_givenInvalidEmailAddress() {
+        assertThatThrownBy(() -> customerRepository.getMemberByEmail("user@test.be"))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessageContaining("Wrong credentials");
     }
 }
